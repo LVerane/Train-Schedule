ecefb95e585f13a994e9e560862dc1c05dc0afd4
Lucio Verane <verane64@gmail.com>
2019-10-02 15:38:11 -0500

    next train and minutes until next train properly working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBNKh7NaVRVHTY8qFliPBRSvA0Z-Zvcr2g",
     authDomain: "train-5e78e.firebaseapp.com",
@@ -8,52 +7,28 @@ const firebaseConfig = {
     messagingSenderId: "76556621900",
     appId: "1:76556621900:web:4c039e6dd1322410113ce9"
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-// initial value
 var train;
 var destination;
 var firstTime;
 var frequency;
 
-// var nextArrival;
-
 $("#submit").on("click", function () {
     event.preventDefault();
 
-    train = $("#train-name").val().trim();
-    destination = $("#destination").val().trim();
-    firstTime = $("#first-time").val().trim();
-    frequency = $("#frequency").val().trim();
-
-    hour = firstTime.split(":")[0]
-    minute = firstTime.split(":")[1]
-    console.log(hour)
-    console.log(minute)
-
-    // var firstTime = moment(userTime, "hh:mm");
-    // var realTime = firstTime._i;
-    //increased time not working
-    // var increasedTime = moment(firstTime).add(frequency, "minutes")
-
-    // date = $("#employ-date").val();
-    // console.log(date);
-    // var randomFormat = "MM/DD/YYYY";
-    // var convertedDate = moment(date, randomFormat);
-    // time = convertedDate.diff(moment(), "month") * -1;
-
-    // rate = $("#employ-rate").val();
-    // paytotal = rate * time;
+    var train = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTime = $("#first-time").val().trim();
+    var frequency = $("#frequency").val().trim();
 
     database.ref().push({
         train: train,
         destination: destination,
         firstTime: firstTime,
         frequency: frequency,
-        // increasedTime : increasedTime,
-        // nextArrival : nextArrival,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 });
@@ -61,17 +36,27 @@ $("#submit").on("click", function () {
 
 database.ref().on("child_added", function (snapshot) {
     var sv = snapshot.val();
+
+    //first time adjusted to be in the past
+    var firstTimeConverted = moment(sv.firstTime, "hh:mm").subtract(1, "day");
+    
+    //difference between times
+    var timeDiff = moment().diff(firstTimeConverted, "minutes");
+
+    //time until next train
+    var tMinutesTillTrain = sv.frequency - timeDiff % sv.frequency;
+
+    //time of arrival of the next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainTime = moment(nextTrain).format("hh:mm");
+
     var newRow = $("<tr>");
-    console.log(sv);
 
     newRow.append($("<td>").text(sv.train));
     newRow.append($("<td>").text(sv.destination));
     newRow.append($("<td>").text(sv.frequency));
-
-    newRow.append($("<td>").text(sv.firstTime));
-    // newRow.append($("<td>").text(sv.increasedTime));// REALTIME + FREQUENCY
-    // newRow.append($("<td>").text(sv.rate));
-    // newRow.append($("<td>").text(sv.paytotal));
+    newRow.append($("<td>").text(nextTrainTime));
+    newRow.append($("<td>").text(tMinutesTillTrain));
 
     $("#table-body").append(newRow);
 
